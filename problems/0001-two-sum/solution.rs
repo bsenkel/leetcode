0b81d7b86c2b8ac74d50fddda6f171e521a627a2
bsenkel <93093303+bsenkel@users.noreply.github.com>
2025-12-13 10:30:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.5 MB (40.59%)

--- problems/0001-two-sum/solution.rs
+++ problems/0001-two-sum/solution.rs
@@ -1,15 +1,12 @@
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut result: Vec<i32> = Vec::new();
-        for i in 0..nums.len() {
-            for j in (i+1)..nums.len() {
-                if nums[i] + nums[j] == target {
-                    result.push(i as i32);
-                    result.push(j as i32);
-                    return result;
-                }
+        let mut map = std::collections::HashMap::new();
+        for (i, &num) in nums.iter().enumerate() {
+            if let Some(&j) = map.get(&(target - num)) {
+                return vec![j as i32, i as i32];
             }
+            map.insert(num, i);
         }
-        result
-    }
+        vec![]
+    }       
 }
