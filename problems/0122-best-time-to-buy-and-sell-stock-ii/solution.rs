efe045584ba4b7fcd9a45cbe34130d240f8e4120
bsenkel <93093303+bsenkel@users.noreply.github.com>
2025-12-18 18:31:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.3 MB (67.20%)

--- problems/0122-best-time-to-buy-and-sell-stock-ii/solution.rs
+++ problems/0122-best-time-to-buy-and-sell-stock-ii/solution.rs
@@ -1,6 +1,7 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let mut max_profit = 0;
+
         for (index, &price) in prices.iter().enumerate() {
             let next_price = prices.get(index + 1);
             let next = match next_price {
@@ -11,6 +12,7 @@ impl Solution {
                 max_profit += *next - price;
             }
         }
+
         max_profit
     }
 }
