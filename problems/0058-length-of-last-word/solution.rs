44ebbaf019b448fc6bbc2f8d4d288b1b49eb7895
bsenkel <93093303+bsenkel@users.noreply.github.com>
2025-12-22 09:33:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (60.17%)

---
+++ problems/0058-length-of-last-word/solution.rs
@@ -0,0 +1,8 @@
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        s.split_whitespace()
+        .last()
+        .map(|word| word.len() as i32)
+        .unwrap_or(0)
+    }
+}
