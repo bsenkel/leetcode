fb0aab8119410eb9e26892a9dcde01eea094fd26
bsenkel <93093303+bsenkel@users.noreply.github.com>
2025-12-23 15:26:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (57.32%)

--- problems/0066-plus-one/solution.rs
+++ problems/0066-plus-one/solution.rs
@@ -1,18 +1,17 @@
 impl Solution {
     pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
-        
         let mut digits = digits;
-        
         for i in (0..digits.len()).rev() {
-        if digits[i] < 9 {
-            digits[i] += 1;
-            return digits;
+            if digits[i] < 9 {
+                digits[i] += 1;
+                return digits;
+            }
+            // if 9 -> 0, and transfer the increment to next digit
+            digits[i] = 0;
         }
-        digits[i] = 0;
-    }
-
-    let mut result = vec![1];
-    result.extend(digits);
-    result
+        // all digits are e.g. [9, 9, 9], therefore a new digit on the left end is needed
+        let mut result = vec![1];
+        result.extend(digits);
+        result
     }
 }
