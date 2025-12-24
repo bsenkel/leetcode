fdb0037674ff292ae061641cf9751a176f8cd9ca
bsenkel <93093303+bsenkel@users.noreply.github.com>
2025-12-24 17:03:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.5 MB (7.99%)

--- problems/0169-majority-element/solution.rs
+++ problems/0169-majority-element/solution.rs
@@ -1,17 +1,15 @@
-use std::collections::HashMap;
-
 impl Solution {
     pub fn majority_element(nums: Vec<i32>) -> i32 {
-        let threshold = nums.len() / 2;
+        let mut candidate = 0;
+        let mut count = 0;
 
-        nums.iter()
-            .fold(HashMap::new(), |mut map, &num| {
-                *map.entry(num).or_insert(0) += 1;
-                map
-            })
-            .into_iter()
-            .find(|(_, count)| *count > threshold)
-            .map(|(num, _)| num)
-            .unwrap_or(0)
+        for &num in &nums {
+            if count == 0 {
+                candidate = num;
+            }
+            count += if num == candidate { 1 } else { -1 };
         }
+
+        candidate
+    }
 }
