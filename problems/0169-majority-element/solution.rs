f4148665758614b92369dcb7c2399b6e68fa8954
bsenkel <93093303+bsenkel@users.noreply.github.com>
2025-12-24 17:12:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.4 MB (40.62%)

--- problems/0169-majority-element/solution.rs
+++ problems/0169-majority-element/solution.rs
@@ -1,8 +1,10 @@
 impl Solution {
     pub fn majority_element(nums: Vec<i32>) -> i32 {
+        // Booyer Moore Voting Algorithm
         let mut candidate = 0;
         let mut count = 0;
 
+        // find candidate
         for &num in &nums {
             if count == 0 {
                 candidate = num;
@@ -10,6 +12,19 @@ impl Solution {
             count += if num == candidate { 1 } else { -1 };
         }
 
-        candidate
+        // verification
+        let mut verification_count = 0;
+        for &num in &nums {
+            if num == candidate {
+                verification_count += 1;
+            }
+        }
+
+        // check if candidate has majority
+        if verification_count > nums.len() / 2 {
+            candidate
+        } else {
+            panic!("No majority element found!")
+        }
     }
 }
