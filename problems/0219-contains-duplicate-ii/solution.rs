8d7ecb0a9ebb257d50fe46dfdefa11306582ad3e
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-01-25 08:25:08 +0000

    [LeetCode Sync] Runtime - 10 ms (69.20%), Memory - 3.6 MB (81.86%)

--- problems/0219-contains-duplicate-ii/solution.rs
+++ problems/0219-contains-duplicate-ii/solution.rs
@@ -1,7 +1,9 @@
+use std::collections::HashSet;
+
 impl Solution {
     pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
         let k = k as usize;
-        let mut seen = std::collections::HashSet::new();
+        let mut seen = HashSet::new();
         for (i, &num) in nums.iter().enumerate() {
             if i > k {
                 seen.remove(&nums[i - k - 1]);
