6dfcc12c05ebe3ad4ece81b8ab3cb6557aac78ec
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-01-31 17:48:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 5.6 MB (62.70%)

--- problems/0344-reverse-string/solution.rs
+++ problems/0344-reverse-string/solution.rs
@@ -1,5 +1,11 @@
 impl Solution {
     pub fn reverse_string(s: &mut Vec<char>) {
-        s.reverse();
+        let mut left = 0;
+        let mut right = s.len() - 1;
+        while left < right {
+            s.swap(left, right);
+            left += 1;
+            right -= 1;
+        }
     }
 }
