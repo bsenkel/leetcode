5c4965bdb3eff9c08331a5532075d2f5f5c54d1c
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-02-01 13:21:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (47.01%)

--- problems/0191-number-of-1-bits/solution.rs
+++ problems/0191-number-of-1-bits/solution.rs
@@ -1,5 +1,12 @@
 impl Solution {
     pub fn hamming_weight(n: i32) -> i32 {
-        n.count_ones() as i32
+        let mut num = n as u32;
+        let mut count = 0;
+
+        while num != 0 {
+            count += (num & 1) as i32;
+            num >>= 1;
+        }
+        count
     }
 }
