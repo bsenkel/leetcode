59de82f7e873b9b9d098a29a4a19a3a09e82b2e5
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-02-01 13:33:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (47.01%)

--- problems/0191-number-of-1-bits/solution.rs
+++ problems/0191-number-of-1-bits/solution.rs
@@ -1,11 +1,14 @@
+// solution 1: use the integrated count_ones() method
+// solution 2: manually check every bit and move bits to the right
+
 impl Solution {
     pub fn hamming_weight(n: i32) -> i32 {
-        let mut num = n as u32;
+        let mut number = n as u32;
         let mut count = 0;
 
-        while num != 0 {
-            count += (num & 1) as i32;
-            num >>= 1;
+        while number != 0 {
+            count += (number & 1) as i32; // check if the last bit is set to 1 via the AND-operator
+            number >>= 1; // shift all bits to the right, therefore check the next bit
         }
         count
     }
