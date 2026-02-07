978afda586b02b023b267bc80af1e3e9b367c09e
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-02-07 08:38:03 +0000

    [LeetCode Sync] Runtime - 2 ms (63.08%), Memory - 2.4 MB (75.38%)

--- problems/0561-array-partition/solution.rs
+++ problems/0561-array-partition/solution.rs
@@ -1,3 +1,7 @@
+// In each pair (a,b) the larger value is wasted, only the minimum counts.
+// The array needs to be sorted first, to minimize the waste across all pairs.
+// Pair numbers that are close in value together.
+
 impl Solution {
     pub fn array_pair_sum(mut nums: Vec<i32>) -> i32 {
         nums.sort();
