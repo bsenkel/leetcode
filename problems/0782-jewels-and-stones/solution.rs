b88e06f90d3eadc32bf394bc158d916d02916e79
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-03-07 17:49:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (62.50%)

--- problems/0782-jewels-and-stones/solution.rs
+++ problems/0782-jewels-and-stones/solution.rs
@@ -1,3 +1,13 @@
+// solution 1: with String.contains() 
+//  - Total: O(n x m)
+//  - Lookup of single char: O(m)
+// solution 2: with HashSet 
+//  - Total: O(n + m)
+//  - Lookup of single char: O(1)
+
+// n = number of stones
+// m = number of jewels
+
 use std::collections::HashSet;
 
 impl Solution {
