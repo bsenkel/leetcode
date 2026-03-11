085760bf05d7a13c39973a874b924ffbbd407cd7
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-03-11 20:17:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (48.72%)

--- problems/2137-final-value-of-variable-after-performing-operations/solution.rs
+++ problems/2137-final-value-of-variable-after-performing-operations/solution.rs
@@ -1,7 +1,6 @@
 impl Solution {
     pub fn final_value_after_operations(operations: Vec<String>) -> i32 {
         let mut number = 0;
-
         for i in 0..operations.len() {
             match operations[i].as_str(){
                 "++X" => number += 1,
@@ -11,7 +10,6 @@ impl Solution {
                 _ => {},
             }
         }
-
         number
     }
 }
