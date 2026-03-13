7e5015d87e171969ced6c21f77527b47cb5f6bce
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-03-13 19:59:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.3 MB (66.67%)

--- problems/2231-find-first-palindromic-string-in-the-array/solution.rs
+++ problems/2231-find-first-palindromic-string-in-the-array/solution.rs
@@ -1,14 +1,8 @@
 impl Solution {
     pub fn first_palindrome(words: Vec<String>) -> String {
-        
-        let mut output: String = "".to_string();
-        for i in 0..words.len(){
-            if words[i].chars().eq(words[i].chars().rev()){
-                output = words[i].clone();
-                break;
-            }
-        }
-
-        output
+        words
+            .into_iter()
+            .find(|word| word.chars().eq(word.chars().rev()))
+            .unwrap_or_default()
     }
 }
