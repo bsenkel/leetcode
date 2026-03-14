8d947931fbbf32c1587d4044816d5169a2fcd6ef
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-03-14 20:08:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (33.33%)

--- problems/3636-check-balanced-string/solution.rs
+++ problems/3636-check-balanced-string/solution.rs
@@ -5,14 +5,12 @@ impl Solution {
 
         for (i, c) in num.chars().enumerate() {
             let digit = c.to_digit(10).unwrap();
-
             if i % 2 == 0 {
                 sum_even += digit;
             } else {
                 sum_odd += digit;
             }
         }
-        
         sum_even == sum_odd
     }
 }
