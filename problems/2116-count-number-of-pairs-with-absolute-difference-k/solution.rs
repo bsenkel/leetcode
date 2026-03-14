f3453999957566f4585529d42e46580d04b1192f
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-03-14 09:28:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (72.41%)

--- problems/2116-count-number-of-pairs-with-absolute-difference-k/solution.rs
+++ problems/2116-count-number-of-pairs-with-absolute-difference-k/solution.rs
@@ -1,15 +1,8 @@
 impl Solution {
     pub fn count_k_difference(nums: Vec<i32>, k: i32) -> i32 {
-        let mut count = 0;
-
-        for i in 0..nums.len() {
-            for j in (i+1)..nums.len(){    
-                let distance = nums[i].abs_diff(nums[j]) as i32;
-                if (distance == k) {
-                    count += 1;
-                }
-            }
-        }
-        count
+        (0..nums.len())
+            .flat_map(|i| (i + 1..nums.len()).map(move |j| (i, j)))
+            .filter(|&(i, j)| nums[i].abs_diff(nums[j]) as i32 == k)
+            .count() as i32
     }
 }
