7756058f6e96c4ac20f013d62489b436708a38a6
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-03-14 10:38:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.3 MB (70.59%)

--- problems/2292-counting-words-with-a-given-prefix/solution.rs
+++ problems/2292-counting-words-with-a-given-prefix/solution.rs
@@ -2,7 +2,7 @@ impl Solution {
     pub fn prefix_count(words: Vec<String>, pref: String) -> i32 {
         words
             .iter()
-            .map(|word| word.starts_with(&pref) as i32)
-            .sum()
+            .filter(|word| word.starts_with(&pref))
+            .count() as i32
     }
 }
