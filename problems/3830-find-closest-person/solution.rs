9a7abdd77a2995eb85dc8a12bc7c998be4a87d74
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-03-15 09:09:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (37.84%)

--- problems/3830-find-closest-person/solution.rs
+++ problems/3830-find-closest-person/solution.rs
@@ -1,11 +1,14 @@
 impl Solution {
     pub fn find_closest(x: i32, y: i32, z: i32) -> i32 {
-        let person1 = (z-x).abs();
-        let person2 = (z-y).abs();
+        let person1 = x.abs_diff(z);
+        let person2 = y.abs_diff(z);
 
-        if (person1 < person2) {return 1;}
-        if (person1 > person2) {return 2;}
-
-        return 0
+        if person1 < person2 {
+            1
+        } else if person1 > person2 {
+            2
+        } else {
+            0
+        } 
     }
 }
