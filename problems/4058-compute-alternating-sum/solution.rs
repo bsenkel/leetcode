50f7248c564180e7d7c7abb362ef32de02066cfd
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-03-15 19:02:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (46.67%)

--- problems/4058-compute-alternating-sum/solution.rs
+++ problems/4058-compute-alternating-sum/solution.rs
@@ -1,18 +1,15 @@
 impl Solution {
     pub fn alternating_sum(nums: Vec<i32>) -> i32 {
         let mut sum = 0;
-
         for (i, num) in nums.into_iter().enumerate(){
             if i % 2 == 0 {
-                sum = sum + num;
+                sum += num;
             }
             else{
-                sum = sum - num;
+                sum -= num;
             }
 
         }
-
         sum
-
     }
 }
