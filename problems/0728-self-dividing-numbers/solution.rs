aabbf14f090cbd155c32bad8f71201312e041db0
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-03-16 19:19:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (11.76%)

--- problems/0728-self-dividing-numbers/solution.rs
+++ problems/0728-self-dividing-numbers/solution.rs
@@ -1,11 +1,23 @@
 impl Solution {
     pub fn self_dividing_numbers(left: i32, right: i32) -> Vec<i32> {
         (left..=right)
-            .filter(|&num| {
-                let s = num.to_string();
-                !s.contains('0') && 
-                s.chars().all(|c| num % (c.to_digit(10).unwrap() as i32) == 0)
-            })
+            .filter(|&num| {Self::check_self_dividing(num)})
             .collect()
+    }
+
+    fn check_self_dividing(num: i32) -> bool {
+        let mut n = num;
+
+        while n > 0 {
+            let digit = n % 10;
+            if digit == 0 {
+                return false;
+            }
+            if num % digit != 0 {
+                return false;
+            }
+            n /= 10;
+        }
+        true
     }
 }
