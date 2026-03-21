66ed3d198d8dd6b85a6411866519ccf61f91f7df
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-03-21 10:10:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (67.86%)

---
+++ problems/1137-height-checker/solution.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn height_checker(heights: Vec<i32>) -> i32 {
+        let mut expected = heights.clone();
+        expected.sort();
+
+        let mut count = 0;
+        for i in 0..heights.len() {
+            if heights[i] != expected[i] {
+                count += 1;
+            }
+        }
+        count
+    }
+}
