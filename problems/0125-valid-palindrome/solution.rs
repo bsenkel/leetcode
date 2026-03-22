555576de6fd5942353d272434cf7700c877c6b26
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-03-22 11:32:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.7 MB (10.55%)

---
+++ problems/0125-valid-palindrome/solution.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        let filtered: Vec<char> = s
+            .chars()
+            .filter(|c| c.is_ascii_alphanumeric())
+            .map(|c| c.to_ascii_lowercase())
+            .collect();
+
+        filtered.iter().zip(filtered.iter().rev()).all(|(a, b)| a == b)
+    }
+}
