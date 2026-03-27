b4fba5f87009b1fe3e99287f69a2c108459edc08
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-03-27 16:06:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (96.30%)

--- problems/1205-defanging-an-ip-address/solution.rs
+++ problems/1205-defanging-an-ip-address/solution.rs
@@ -1,6 +1,15 @@
 impl Solution {
     pub fn defang_i_paddr(address: String) -> String {
-        let result = address.replace(".", "[.]");
+        let mut result = String::with_capacity(address.len() + 2 * address.matches('.').count());
+
+        for ch in address.chars() {
+            if ch == '.' {
+                result.push_str("[.]");
+            } else {
+                result.push(ch);
+            }
+        }
+
         result
     }
 }
