d0e319f3e5093ae20d2ebfc5963d8233de829291
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-03-28 08:51:20 +0000

    [LeetCode Sync] Runtime - 1569 ms (6.90%), Memory - 3.7 MB (82.76%)

---
+++ problems/0315-count-of-smaller-numbers-after-self/solution.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn count_smaller(nums: Vec<i32>) -> Vec<i32> {
+        let mut result = Vec::with_capacity(nums.len());
+
+        for i in 0..nums.len() {
+            let mut count = 0;
+
+            for x in nums[i + 1..].iter() {
+                if *x < nums[i] {
+                    count += 1;
+                }
+            }
+            
+            result.push(count);
+        }
+
+        result
+    }
+}
