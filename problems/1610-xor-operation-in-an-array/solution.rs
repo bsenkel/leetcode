a17cae54cf10d1e1e39a9fa5b78781484c9a6f7e
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-04-03 07:43:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (48.00%)

--- problems/1610-xor-operation-in-an-array/solution.rs
+++ problems/1610-xor-operation-in-an-array/solution.rs
@@ -1,12 +1,8 @@
 impl Solution {
     pub fn xor_operation(n: i32, start: i32) -> i32 {
-        let mut result = 0;
-
-        for i in 0..n {
-            let val = start + 2 * i;
-            result ^= val;
-        }
-
-        result
+        (0..n)
+            .map(|i| start + 2 * i)
+            .reduce(|acc, x| acc ^ x)
+            .unwrap_or(0)
     }
 }
