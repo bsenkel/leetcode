f4542d997f934a19b3975ac73b3b76114ba6440d
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-04-05 17:54:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (100.00%)

--- problems/3582-find-indices-of-stable-mountains/solution.rs
+++ problems/3582-find-indices-of-stable-mountains/solution.rs
@@ -2,11 +2,7 @@ impl Solution {
     pub fn stable_mountains(height: Vec<i32>, threshold: i32) -> Vec<i32> {
         let mut output = Vec::new();
 
-        for (i,n) in height.iter().enumerate() {
-            if i == 0 { 
-                continue; 
-            }
-
+        for i in 1..height.len() {
             if height[i - 1] > threshold {
                 output.push(i as i32);
             }
