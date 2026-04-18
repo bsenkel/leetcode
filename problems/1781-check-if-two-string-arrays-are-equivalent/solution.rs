bb2b23757ecffbd5c69ab45c9695518fd4b15cbf
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-04-18 07:51:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.4 MB (13.79%)

--- problems/1781-check-if-two-string-arrays-are-equivalent/solution.rs
+++ problems/1781-check-if-two-string-arrays-are-equivalent/solution.rs
@@ -2,7 +2,6 @@ impl Solution {
     pub fn array_strings_are_equal(word1: Vec<String>, word2: Vec<String>) -> bool {
         let a = word1.join("");
         let b = word2.join("");
-        if a == b {true}
-        else {false}
+        a == b
     }
 }
