3405b8e465cbe78f57b61f7eedab586ff2183515
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-04-20 17:53:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (81.63%)

--- problems/0742-to-lower-case/solution.rs
+++ problems/0742-to-lower-case/solution.rs
@@ -1,5 +1,10 @@
 impl Solution {
     pub fn to_lower_case(s: String) -> String {
-        s.to_lowercase()
+        s.chars()
+            .map(|c| match c {
+                'A'..='Z' => (c as u8 + 32) as char,
+                _ => c,
+            })
+            .collect()
     }
 }
