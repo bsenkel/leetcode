e7b21dcb43648fd564f4c32474a4dea3fcf95c19
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-04-20 17:43:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (81.63%)

--- problems/0742-to-lower-case/solution.rs
+++ problems/0742-to-lower-case/solution.rs
@@ -1,16 +1,5 @@
-// solution 1: use the integrated to_lowercase() method
-// solution 2: manually convert with keeping the difference of 32 in mind (e.g. 'A' = 65, 'a' = 97)
-
 impl Solution {
     pub fn to_lower_case(s: String) -> String {
-        s.chars()
-        .map(|c| {
-            if c >= 'A' && c <= 'Z' {
-                ((c as u8) + 32) as char
-            } else {
-                c
-            }
-        })
-        .collect()
+        s.to_lowercase()
     }
 }
