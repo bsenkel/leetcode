2ca6f618b9f8601f3f0323c44f832dbf25241bef
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-04-23 19:43:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (14.29%)

--- problems/2235-capitalize-the-title/solution.rs
+++ problems/2235-capitalize-the-title/solution.rs
@@ -1,25 +1,21 @@
 impl Solution {
     pub fn capitalize_title(title: String) -> String {
-        let mut result: Vec<String> = Vec::new();
-
-        for word in title.split(' ') {
-            if word.len() <= 2 {
-                result.push(word.to_lowercase());
-            }
-            else
-            {
-                let mut s = String::new();
-                for (i, c) in word.chars().enumerate() {
-                    if i == 0 {
-                        s.push(c.to_ascii_uppercase());
-                    } else {
-                        s.push(c.to_ascii_lowercase());
+        title
+            .split(' ')
+            .map(|word| {
+                if word.len() <= 2 {
+                    word.to_lowercase()
+                } else {
+                    let mut chars = word.chars();
+                    match chars.next() {
+                        None => String::new(),
+                        Some(first) => {
+                            format!("{}{}", first.to_uppercase(), chars.as_str().to_lowercase())
+                        }
                     }
                 }
-                result.push(s);
-            }
-        }
-
-        result.join(" ")
+            })
+            .collect::<Vec<String>>()
+            .join(" ")
     }
 }
