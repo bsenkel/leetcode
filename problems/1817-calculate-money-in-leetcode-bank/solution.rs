b28a39e612d69ff972a69fafa2ce2d3ca560bd57
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-04-27 20:40:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (46.34%)

--- problems/1817-calculate-money-in-leetcode-bank/solution.rs
+++ problems/1817-calculate-money-in-leetcode-bank/solution.rs
@@ -1,16 +1,15 @@
 impl Solution {
     pub fn total_money(n: i32) -> i32 {
-        let mut monday = 0;
+        let mut week_offset = 0;
         let mut total = 0;
 
         for i in 1..=n{
             let day_in_week = (i - 1) % 7 + 1;
-            total += day_in_week + monday;
+            total += day_in_week + week_offset;
 
             if i % 7 == 0{
-                monday += 1;
-            } 
-
+                week_offset += 1;
+            }
         }
 
         total
