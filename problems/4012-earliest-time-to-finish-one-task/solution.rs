787cb05daef57625f9d2c6419d806ff9ecebf9c0
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-01 09:01:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.3 MB (42.11%)

---
+++ problems/4012-earliest-time-to-finish-one-task/solution.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn earliest_time(tasks: Vec<Vec<i32>>) -> i32 {
+        let mut sums = Vec::new();
+        for task in &tasks{
+            let sum = task[0] + task[1];
+            sums.push(sum);
+        }
+        *sums.iter().min().unwrap()
+    }
+}
