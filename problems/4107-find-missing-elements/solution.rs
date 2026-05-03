897075939347429115e237a45a0f95340b8d0065
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-03 10:30:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (71.43%)

--- problems/4107-find-missing-elements/solution.rs
+++ problems/4107-find-missing-elements/solution.rs
@@ -1,8 +1,10 @@
 use std::collections::HashSet;
+
 impl Solution {
     pub fn find_missing_elements(nums: Vec<i32>) -> Vec<i32> {
         let min = *nums.iter().min().unwrap();
         let max = *nums.iter().max().unwrap();
+        
         let elements: HashSet<i32> = nums.into_iter().collect();
 
         (min..=max)
