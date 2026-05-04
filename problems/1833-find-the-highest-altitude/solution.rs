5adccf30f6cb2571eafe35cfb639c96ea19bdbb8
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-04 20:03:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (62.64%)

--- problems/1833-find-the-highest-altitude/solution.rs
+++ problems/1833-find-the-highest-altitude/solution.rs
@@ -1,15 +1,15 @@
 impl Solution {
     pub fn largest_altitude(gain: Vec<i32>) -> i32 {
-        let mut altitude = 0;
-        let mut tmp = 0;
+        let mut max_altitude = 0;
+        let mut current = 0;
 
         for g in gain{
-            tmp += g;
-            if tmp > altitude{
-                altitude = tmp;
+            current += g;
+            if current > max_altitude{
+                max_altitude = current;
             }
         }
 
-        altitude
+        max_altitude
     }
 }
