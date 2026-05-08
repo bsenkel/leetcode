22078f60b2058d4403ba19bc0b120e81d2189a18
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-08 19:01:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 4.2 MB (75.00%)

--- problems/1899-count-items-matching-a-rule/solution.rs
+++ problems/1899-count-items-matching-a-rule/solution.rs
@@ -1,16 +1,20 @@
 impl Solution {
     pub fn count_matches(items: Vec<Vec<String>>, rule_key: String, rule_value: String) -> i32 {
-        items.iter().filter(|item| {
-            if let [typ, color, name] = item.as_slice() {
-                match rule_key.as_str() {
-                    "type" => *typ == rule_value,
-                    "color" => *color == rule_value,
-                    "name" => *name == rule_value,
-                    _ => false,
+        let idx = match rule_key.as_str() {
+            "type"  => 0,
+            "color" => 1,
+            "name"  => 2,
+            _       => return 0,
+        };
+
+        let mut count = 0;
+        for item in &items {
+            if let Some(val) = item.get(idx) {
+                if val == &rule_value {
+                    count += 1;
                 }
-            } else {
-                false
             }
-        }).count().try_into().unwrap()
+        }
+        count as i32
     }
 }
