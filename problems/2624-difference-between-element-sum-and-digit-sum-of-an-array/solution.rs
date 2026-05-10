7aca0f5d950da6dc041540b02f25eb68cf50f5c4
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-10 09:32:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (71.03%)

--- problems/2624-difference-between-element-sum-and-digit-sum-of-an-array/solution.rs
+++ problems/2624-difference-between-element-sum-and-digit-sum-of-an-array/solution.rs
@@ -1,17 +1,12 @@
 impl Solution {
     pub fn difference_of_sum(nums: Vec<i32>) -> i32 {      
-        let mut element_sum = 0;
-        let mut digit_sum = 0;
+        let element_sum: i32 = nums.iter().sum();
         
-        for num in nums{
-            element_sum += num;
-
-            let mut n = num;
-            while n > 0 {
-                digit_sum += n % 10;
-                n /= 10;
-            }
-        }
+        let digit_sum: i32 = nums
+            .iter()
+            .flat_map(|n| n.to_string().into_bytes())
+            .map(|b| (b - b'0') as i32)
+            .sum();
 
         element_sum.abs_diff(digit_sum) as i32
     }
