ddbf36de53535504608d6396b4cf5607d1a1240b
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-14 08:48:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (100.00%)

--- problems/2486-most-frequent-even-element/solution.rs
+++ problems/2486-most-frequent-even-element/solution.rs
@@ -3,14 +3,19 @@ use std::collections::HashMap;
 impl Solution {
     pub fn most_frequent_even(nums: Vec<i32>) -> i32 {
         let mut frequent: HashMap<i32, i32> = HashMap::new();
-        let mut result = -1;
-        let mut max_count = 0;
-
+        
         for n in nums {
             if n % 2 == 0 {
                 *frequent.entry(n).or_insert(0) += 1;
             }
         }
+
+        if frequent.is_empty() {
+            return -1;
+        }
+
+        let mut result = -1;
+        let mut max_count = 0;
 
         for (&num, &count) in &frequent {
             if count > max_count || (count == max_count && num < result) {
