0b78ca848dba589e2381ba71cb124834ee783d22
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-14 07:01:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.6 MB (10.53%)

--- problems/0455-assign-cookies/solution.rs
+++ problems/0455-assign-cookies/solution.rs
@@ -1,20 +1,20 @@
 impl Solution {
     pub fn find_content_children(g: Vec<i32>, s: Vec<i32>) -> i32 {
-        let mut greed = g;
-        let mut size = s;
+        let mut g = g;
+        let mut s = s;
 
-        greed.sort();
-        size.sort();
+        g.sort(); // greed factor of children
+        s.sort(); // cookie size
 
         let (mut child, mut cookie) = (0, 0);
 
-        while child < greed.len() && cookie < size.len() {
-            if size[cookie] >= greed[child] {
+        while child < g.len() && cookie < s.len() {
+            if s[cookie] >= g[child] {
                 child += 1; // satisfied child
             }
             cookie += 1; // try next cookie
         }
 
-        child as i32
+        child as i32 // number of satisfied children
     }
 }
