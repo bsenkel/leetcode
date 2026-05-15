08fe5cad56b441423758ee9a5712bd5d380cfcd1
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-15 08:29:37 +0000

    [LeetCode Sync] Runtime - 4 ms (39.29%), Memory - 4 MB (7.14%)

--- problems/0287-find-the-duplicate-number/solution.rs
+++ problems/0287-find-the-duplicate-number/solution.rs
@@ -2,7 +2,10 @@ use std::collections::HashSet;
 
 impl Solution {
     pub fn find_duplicate(nums: Vec<i32>) -> i32 {
-        let mut seen = HashSet::new();
-        *nums.iter().find(|&&i| !seen.insert(i)).unwrap()
+        let mut seen = HashSet::with_capacity(nums.len());
+
+        *nums
+            .iter()
+            .find(|&&i| !seen.insert(i)).unwrap()
     }
 }
