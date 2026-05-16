1d228f1a48b18fd8d894e2e4eb8f2fbea631f3bd
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-16 07:12:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (58.82%)

--- problems/4009-bitwise-or-of-even-numbers-in-an-array/solution.rs
+++ problems/4009-bitwise-or-of-even-numbers-in-an-array/solution.rs
@@ -1,6 +1,8 @@
 impl Solution {
     pub fn even_number_bitwise_o_rs(nums: Vec<i32>) -> i32 {
-        let result = nums.iter().filter(|&&x| x % 2 == 0).fold(0, |acc, x| acc | x);
-        result
+        nums
+            .iter()
+            .filter(|&&x| x % 2 == 0)
+            .fold(0, |acc, x| acc | x)
     }
 }
