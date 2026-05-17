2005e8a5e65c53079ed886ffd8a42d5cc61df79a
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-17 07:13:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (50.70%)

--- problems/2556-convert-the-temperature/solution.rs
+++ problems/2556-convert-the-temperature/solution.rs
@@ -3,9 +3,6 @@ impl Solution {
         let kelvin = celsius + 273.15;
         let fahrenheit = celsius * 1.80 + 32.00;
 
-        let mut ans: Vec<f64> = Vec::new();
-        ans.push(kelvin);
-        ans.push(fahrenheit);
-        ans
+        vec![kelvin, fahrenheit]
     }
 }
