4d976fb851657123e1629818ce4555abe4473200
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-20 18:13:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (95.83%)

--- problems/3172-divisible-and-non-divisible-sums-difference/solution.rs
+++ problems/3172-divisible-and-non-divisible-sums-difference/solution.rs
@@ -1,17 +1,16 @@
 impl Solution {
     pub fn difference_of_sums(n: i32, m: i32) -> i32 {
-        let mut num1 = 0;
-        let mut num2 = 0;
+        let mut result = 0;
 
         for i in 1..=n{
-            if i % m != 0 {
-                num1 += i;
+            if i % m == 0 {
+                result -= i;
             }
-            else{
-                num2 += i;
+            else {
+                result += i;
             }
         }
 
-        num1 - num2
+        result
     }
 }
