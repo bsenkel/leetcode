c991817897ef599dbcb3c13829ae0bdfd2a5c433
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-22 07:15:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (19.44%)

--- problems/2876-number-of-employees-who-met-the-target/solution.rs
+++ problems/2876-number-of-employees-who-met-the-target/solution.rs
@@ -1,13 +1,5 @@
 impl Solution {
     pub fn number_of_employees_who_met_target(hours: Vec<i32>, target: i32) -> i32 {
-        let mut targets = 0;
-
-        for i in hours{
-            if i >= target {
-                targets += 1;
-            }
-        }
-
-        targets
+        hours.iter().filter(|&&i| i >= target).count() as i32
     }
 }
