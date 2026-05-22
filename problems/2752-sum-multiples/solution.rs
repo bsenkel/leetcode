90f3dd7d4984b3ba5e15e3881b8f4921a4964683
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-22 06:19:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (59.09%)

--- problems/2752-sum-multiples/solution.rs
+++ problems/2752-sum-multiples/solution.rs
@@ -1,5 +1,7 @@
 impl Solution {
     pub fn sum_of_multiples(n: i32) -> i32 {
-        (1..=n).filter(|i| i % 3 == 0 || i % 5 == 0 || i % 7 == 0).sum()
+        (1..=n)
+            .filter(|i| i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+            .sum()
     }
 }
