ed21af2562488d3b82a076e6f998f9d024de9719
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-24 07:48:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.3 MB (63.08%)

--- problems/0014-longest-common-prefix/solution.rs
+++ problems/0014-longest-common-prefix/solution.rs
@@ -1,17 +1,15 @@
 impl Solution {
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
-        if strs.is_empty() {
-          return String::new();
-        }
+        let Some(mut prefix) = strs.first().cloned() else {
+            return String::new();
+        };
 
-        let mut prefix = strs[0].clone();
-
-        for s in &strs[1..] {
+        for s in strs.iter().skip(1) {
             while !s.starts_with(&prefix) {
                 prefix.pop();
 
                 if prefix.is_empty() {
-                    return String::new();
+                    return prefix;
                 }
             }
         }
