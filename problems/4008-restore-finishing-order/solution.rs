28057efc9828e969b341cb3f0f02f08d3d745ac2
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-25 08:47:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (35.00%)

--- problems/4008-restore-finishing-order/solution.rs
+++ problems/4008-restore-finishing-order/solution.rs
@@ -1,10 +1,14 @@
 impl Solution {
     pub fn recover_order(order: Vec<i32>, friends: Vec<i32>) -> Vec<i32> {
-        let mut finish = Vec::new();
+        let mut finish = Vec::with_capacity(friends.len());
 
         for participant in order{
             if friends.contains(&participant){
                 finish.push(participant);
+            }
+
+            if finish.len() == friends.len() {
+                break;
             }
         }
 
