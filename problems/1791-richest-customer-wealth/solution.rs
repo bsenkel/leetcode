957bcb52d5d081823a766dc3fe8218c8fd995540
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-29 08:31:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.4 MB (20.45%)

--- problems/1791-richest-customer-wealth/solution.rs
+++ problems/1791-richest-customer-wealth/solution.rs
@@ -1,19 +1,9 @@
 impl Solution {
     pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
-        let mut max_wealth = 0;
-
-        for customer in &accounts{
-            let mut wealth = 0;
-
-            for money in customer {
-                wealth += money; 
-            }
-
-            if max_wealth < wealth {
-                max_wealth = wealth;
-            }
-        }
-
-        max_wealth
+        accounts
+            .iter()
+            .map(|customer| customer.iter().sum())
+            .max()
+            .unwrap_or(0)
     }
 }
