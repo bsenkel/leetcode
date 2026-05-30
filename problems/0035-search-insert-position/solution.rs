2a19eda07f912f5b299fe190da0fd56d97f7d8c3
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-30 07:55:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2 MB (100.00%)

--- problems/0035-search-insert-position/solution.rs
+++ problems/0035-search-insert-position/solution.rs
@@ -1,21 +1,11 @@
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        let mut index = 0;
-
         for (pos, num) in nums.iter().enumerate(){
-            if *num == target{
-                index = pos;
-                break;
-            }
-            else if *num > target{
-                index = pos;
-                break;
-            }
-            else if pos == nums.len() - 1 {
-                index = pos + 1;
+            if *num >= target{
+                return pos as i32;
             }
         }
 
-        index as i32
+        nums.len() as i32
     }
 }
