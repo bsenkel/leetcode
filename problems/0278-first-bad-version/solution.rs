1ea0e4b2ae77bb062fe511d32a5595ee986d78d5
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-05-31 07:30:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (83.33%)

--- problems/0278-first-bad-version/solution.rs
+++ problems/0278-first-bad-version/solution.rs
@@ -1,7 +1,3 @@
-// The API isBadVersion is defined for you.
-// isBadVersion(version:i32)-> bool;
-// to call it use self.isBadVersion(version)
-
 impl Solution {
     pub fn first_bad_version(&self, n: i32) -> i32 {
         let mut left = 1;
