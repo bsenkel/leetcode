2b86669eeb3f1a857e1a33912fbebf2ebf309b21
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-06-02 18:43:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (55.95%)

---
+++ problems/0119-pascals-triangle-ii/solution.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    pub fn get_row(row_index: i32) -> Vec<i32> {
+        let mut triangle: Vec<Vec<i32>> = Vec::new();
+
+        for row in 0..=row_index {
+            let mut new_row: Vec<i32> = Vec::new();
+
+            for col in 0..=row {
+                if col == 0 || col == row {
+                    new_row.push(1);
+                } else {
+                    let top_left = triangle[(row - 1) as usize][(col - 1) as usize];
+                    let top_right = triangle[(row - 1) as usize][col as usize];
+                    new_row.push(top_left + top_right);
+                }
+            }
+
+            triangle.push(new_row);
+        }
+
+        triangle[(row_index) as usize].clone()
+    }
+}
