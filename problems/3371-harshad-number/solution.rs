7caed7b17d87e36e6e3824ab17592ef1f46bab25
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-06-06 18:54:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (93.75%)

---
+++ problems/3371-harshad-number/solution.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn sum_of_the_digits_of_harshad_number(x: i32) -> i32 {
+        let mut n = x;
+        let mut sum = 0;
+
+        while n > 0 {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        if x % sum == 0 {
+            sum
+        }
+        else {
+            -1
+        }
+    }
+}
