1d7d6723c2d3173089136141f818dea1e2e6c8fd
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-06-10 18:39:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (23.38%)

---
+++ problems/4280-count-digit-appearances/solution.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn count_digit_occurrences(nums: Vec<i32>, digit: i32) -> i32 {
+        let mut count = 0;
+
+        for num in nums{
+            let mut number = num;
+            while number > 0{
+                let x = number % 10;
+                if x == digit{
+                    count += 1;
+                }
+                number /= 10;
+            }
+        }
+
+        count
+    }
+}
