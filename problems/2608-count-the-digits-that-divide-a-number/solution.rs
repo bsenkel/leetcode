88f14d287e2ed4f8b4721c31219003b52de9a468
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-06-11 18:29:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (35.71%)

---
+++ problems/2608-count-the-digits-that-divide-a-number/solution.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn count_digits(num: i32) -> i32 {
+        let mut count = 0;
+
+        let mut x = num;
+        while x > 0{
+            let n = x % 10;
+            if num % n == 0{
+                count += 1;
+            }
+            x /= 10;
+        }
+
+        count
+    }
+}
