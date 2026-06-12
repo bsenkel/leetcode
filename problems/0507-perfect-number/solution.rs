bafe4e8e131d8bcda299f0049e63ed124a666c9d
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-06-12 07:18:27 +0000

    [LeetCode Sync] Runtime - 1052 ms (12.50%), Memory - 2.2 MB (6.25%)

---
+++ problems/0507-perfect-number/solution.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn check_perfect_number(num: i32) -> bool {
+        let mut sum = 0;
+
+        for i in 1..=num/2 {
+            if num % i == 0{
+                sum += i;
+            }
+        }
+
+        if sum == num{
+            return true;
+        }
+
+        false
+    }
+}
