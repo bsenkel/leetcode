1708347540e8a8dd200babc5810a310f19139583
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-06-13 11:59:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (45.83%)

---
+++ problems/0231-power-of-two/solution.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn is_power_of_two(n: i32) -> bool {
+        if n <= 0{
+            return false;
+        }
+
+        let mut x = n;
+        while x % 2 == 0{
+            x /= 2;
+        }
+
+        return x == 1
+    }
+}
