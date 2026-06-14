e78ca6cceddb7614bebdda4ff9386384ac57e12b
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-06-14 09:51:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (95.16%)

--- problems/0326-power-of-three/solution.rs
+++ problems/0326-power-of-three/solution.rs
@@ -1,11 +1,11 @@
 impl Solution {
     pub fn is_power_of_three(n: i32) -> bool {
-        if n <= 0{
+        if n <= 0 {
             return false;
         }
 
         let mut x = n;
-        while x % 3 == 0{
+        while x % 3 == 0 {
             x /= 3;
         }
 
