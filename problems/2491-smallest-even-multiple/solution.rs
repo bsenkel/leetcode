b17fdadfbe62bd88c894ede0a8302f4dfdd89bc9
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-06-16 20:59:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (52.00%)

--- problems/2491-smallest-even-multiple/solution.rs
+++ problems/2491-smallest-even-multiple/solution.rs
@@ -2,8 +2,7 @@ impl Solution {
     pub fn smallest_even_multiple(n: i32) -> i32 {
         if n % 2 == 0 {
             n
-        }
-        else {
+        } else {
             2 * n
         }
     }
