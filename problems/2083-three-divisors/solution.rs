e4fb003fdef6a36724f181dd92b7f38fbb3b8448
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-06-17 18:57:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2 MB (100.00%)

---
+++ problems/2083-three-divisors/solution.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn is_three(n: i32) -> bool {
+        let mut count = 1;
+
+        for i in 1..=n/2 {
+            if (n % i == 0) {
+                count += 1;
+            }
+
+            if (count > 3) { return false; }
+        }
+
+        return count == 3;
+    }
+}
