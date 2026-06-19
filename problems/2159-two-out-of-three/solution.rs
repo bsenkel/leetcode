87c6e7735a413fc2db287570441badce3f23c319
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-06-19 19:58:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.4 MB (0.00%)

--- problems/2159-two-out-of-three/solution.rs
+++ problems/2159-two-out-of-three/solution.rs
@@ -1,5 +1,4 @@
-use std::collections::HashSet;
-use std::collections::HashMap;
+use std::collections::{HashSet, HashMap};
 
 impl Solution {
     pub fn two_out_of_three(nums1: Vec<i32>, nums2: Vec<i32>, nums3: Vec<i32>) -> Vec<i32> {
