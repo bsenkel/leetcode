e03ad2d01cc0629f3ed69e2d14d8e8aaa6a84b46
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-06-20 07:34:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.4 MB (57.14%)

---
+++ problems/0661-image-smoother/solution.rs
@@ -0,0 +1,32 @@
+impl Solution {
+    pub fn image_smoother(img: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let rows = img.len();
+        let cols = img[0].len();
+
+        let mut result = vec![vec![0; cols]; rows];
+
+        for row in 0..rows {
+            for col in 0..cols {
+                let mut sum = 0;
+                let mut count = 0;
+
+                let row_start = row.saturating_sub(1);
+                let row_end = (row + 1).min(rows - 1);
+
+                let col_start = col.saturating_sub(1);
+                let col_end = (col + 1).min(cols - 1);
+
+                for neighbor_row in row_start..=row_end {
+                    for neighbor_col in col_start..=col_end {
+                        sum += img[neighbor_row][neighbor_col];
+                        count += 1;
+                    }
+                }
+
+                result[row][col] = sum / count;
+            }
+        }
+        
+        result
+    }
+}
