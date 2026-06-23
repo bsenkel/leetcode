d7fbaee6d262da79ecdc8964161762f2f42e93c0
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-06-23 18:38:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (95.00%)

--- problems/2427-first-letter-to-appear-twice/solution.rs
+++ problems/2427-first-letter-to-appear-twice/solution.rs
@@ -3,13 +3,14 @@ use std::collections::HashMap;
 impl Solution {
     pub fn repeated_character(s: String) -> char {
         let mut counts: HashMap<char, usize> = HashMap::new();
-        let mut letter: char = ' ';
+        let mut letter = ' ';
 
         for c in s.chars() {
-            *counts.entry(c).or_insert(0) += 1;
+            let count = counts.entry(c).or_insert(0);
+            *count += 1;
 
-            if let Some(key) = counts.iter().find(|&(_, &v)| v == 2).map(|(k, _)| k) {
-                letter = *key;
+            if *count == 2 {
+                letter = c;
                 break;
             }
         }
