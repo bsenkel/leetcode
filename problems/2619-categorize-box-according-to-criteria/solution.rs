e5fdc1de0896c79b695c3995dbdcc9796cb746c7
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-07-03 09:22:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (16.67%)

--- problems/2619-categorize-box-according-to-criteria/solution.rs
+++ problems/2619-categorize-box-according-to-criteria/solution.rs
@@ -1,7 +1,7 @@
 impl Solution {
     pub fn categorize_box(length: i32, width: i32, height: i32, mass: i32) -> String {
         const THRESH1: i32 = 10_000;
-        const THRESH2: i64 = 10_00000000;
+        const THRESH2: i64 = 1_000_000_000;
 
         let mut is_bulky = false;
         let mut is_heavy = false;
