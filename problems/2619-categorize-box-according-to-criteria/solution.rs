df4c45ea0fa1ee40ddec81180e762f0e3ef38720
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-07-03 09:20:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (16.67%)

---
+++ problems/2619-categorize-box-according-to-criteria/solution.rs
@@ -0,0 +1,37 @@
+impl Solution {
+    pub fn categorize_box(length: i32, width: i32, height: i32, mass: i32) -> String {
+        const THRESH1: i32 = 10_000;
+        const THRESH2: i64 = 10_00000000;
+
+        let mut is_bulky = false;
+        let mut is_heavy = false;
+
+        let volume = i64::from(length) * i64::from(width) * i64::from(height);
+
+        if length >= THRESH1
+            || width >= THRESH1
+            || height >= THRESH1
+            || volume >= THRESH2
+        {
+            is_bulky = true;
+        }
+
+        if mass >= 100 {
+            is_heavy = true;
+        }
+
+        if is_bulky && is_heavy {
+            return "Both".to_string();
+        }
+
+        if is_bulky && !is_heavy {
+            return "Bulky".to_string();
+        }
+
+        if !is_bulky && is_heavy {
+            return "Heavy".to_string();
+        }
+
+        "Neither".to_string()
+    }
+}
