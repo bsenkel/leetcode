5e2d5f275099be55e1abb6b7794e8fe3f0fa91ae
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-07-04 08:04:45 +0000

    [LeetCode Sync] Runtime - 3 ms (35.35%), Memory - 2.1 MB (93.94%)

--- problems/0383-ransom-note/solution.rs
+++ problems/0383-ransom-note/solution.rs
@@ -2,36 +2,36 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn can_construct(ransom_note: String, magazine: String) -> bool {
-        let mut count1 = HashMap::new();
-        let mut count2 = HashMap::new();
+        let mut magazine_count = HashMap::new();
+        let mut ransom_count = HashMap::new();
 
         for c in magazine.chars() {
-            *count1.entry(c).or_insert(0) += 1;
+            *magazine_count.entry(c).or_insert(0) += 1;
         }
 
         for c in ransom_note.chars() {
-            *count2.entry(c).or_insert(0) += 1;
+            *ransom_count.entry(c).or_insert(0) += 1;
         }
 
-        let mut can_do = false;
+        let mut can_construct = false;
 
-        for (key, value) in count2.iter() {
-            match count1.get(key) {
+        for (key, value) in ransom_count.iter() {
+            match magazine_count.get(key) {
                 Some(v) => {
                     if value <= v {
-                        can_do = true;
+                        can_construct = true;
                     } else {
-                        can_do = false;
+                        can_construct = false;
                         break;
                     }
                 }
                 None => {
-                    can_do = false;
+                    can_construct = false;
                     break;
                 },
             }
         }
 
-        can_do
+        can_construct
     }
 }
