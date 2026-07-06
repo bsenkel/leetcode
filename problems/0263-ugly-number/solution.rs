e0440d79bcd6435adb34ba076c44026f1a39e9a2
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-07-06 18:48:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (9.09%)

--- problems/0263-ugly-number/solution.rs
+++ problems/0263-ugly-number/solution.rs
@@ -3,14 +3,15 @@ impl Solution {
         if n == 1 {
             return true;
         }
+        
         if n <= 0 {
             return false;
         }
 
         let mut number = n;
 
-        for factor in [2, 3, 5]{
-            while number % factor == 0{
+        for factor in [2, 3, 5] {
+            while number % factor == 0 {
                 number /= factor;
             }
         }
