cfc3ae398b844e9bd3930fd6c4c48c7543bb75d1
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-07-12 06:21:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (30.00%)

--- problems/2298-count-integers-with-even-digit-sum/solution.rs
+++ problems/2298-count-integers-with-even-digit-sum/solution.rs
@@ -2,13 +2,14 @@ impl Solution {
     pub fn count_even(num: i32) -> i32 {
         let mut count = 0;
 
-        for i in 1..=num {
-            let number = i.to_string();
+        for number in 1..=num {
             let mut sum = 0;
-            for c in number.chars() {
-                let d = c.to_digit(10).unwrap();
-                sum += d as i32;
+
+            for c in number.to_string().chars() {
+                let digit = c.to_digit(10).unwrap();
+                sum += digit as i32;
             }
+
             if sum % 2 == 0 {
                 count += 1;
             }
