5a9c218565ba645c17c652204141841e5d7637f8
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-07-13 20:42:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (30.77%)

--- problems/2298-count-integers-with-even-digit-sum/solution.rs
+++ problems/2298-count-integers-with-even-digit-sum/solution.rs
@@ -1,8 +1,6 @@
 impl Solution {
     pub fn count_even(num: i32) -> i32 {
-        (1..=num)
-          .filter(|&n| Self::digit_sum(n) % 2 == 0)
-          .count() as i32
+        (1..=num).filter(|&n| Self::digit_sum(n) % 2 == 0).count() as i32
     }
 
     fn digit_sum(mut n: i32) -> i32 {
