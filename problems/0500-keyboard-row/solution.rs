81324e16f8ff1423f1d42af83a6d676cc717bb72
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-07-14 18:52:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (100.00%)

---
+++ problems/0500-keyboard-row/solution.rs
@@ -0,0 +1,18 @@
+use std::collections::HashSet;
+impl Solution {
+    pub fn find_words(words: Vec<String>) -> Vec<String> {
+        let rows: [HashSet<char>; 3] = [
+          "qwertyuiop".chars().collect(),
+          "asdfghjkl".chars().collect(),
+          "zxcvbnm".chars().collect(),
+        ];
+
+        words
+            .into_iter()
+            .filter(|word| {
+                let lower: Vec<char> = word.chars().map(|c| c.to_ascii_lowercase()).collect();
+                rows.iter().any(|row| lower.iter().all(|c| row.contains(c)))
+            })
+            .collect()
+    }
+}
