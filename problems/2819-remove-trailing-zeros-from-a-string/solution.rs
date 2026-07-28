ad10cf43e1d90a55b736d2c64fce6dc6b59674b5
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-07-28 18:05:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.5 MB (11.11%)

--- problems/2819-remove-trailing-zeros-from-a-string/solution.rs
+++ problems/2819-remove-trailing-zeros-from-a-string/solution.rs
@@ -1,20 +1,14 @@
 impl Solution {
     pub fn remove_trailing_zeros(num: String) -> String {
-        let mut iter = num.chars().rev();
-
-        let mut count = 0;
-        let mut flag = true;
+        let mut remove = 0;
         
-        while flag{
-            if iter.next() == Some('0') {
-                count += 1;
-            } else {
-                flag = false;
+        for c in num.chars().rev() {
+            if c != '0' {
+                break;
             }
+            remove += 1;
         }
-        
-        let result: String = num.chars().take(num.chars().count() - count).collect();
 
-        result
+        num.chars().take(num.chars().count() - remove).collect()
     }
 }
