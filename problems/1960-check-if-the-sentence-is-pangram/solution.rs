3a61ccd98d3c1c2e89b2122f6dd381a7a86e565e
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-07-31 08:24:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (20.00%)

--- problems/1960-check-if-the-sentence-is-pangram/solution.rs
+++ problems/1960-check-if-the-sentence-is-pangram/solution.rs
@@ -1,8 +1,12 @@
 use std::collections::HashSet;
+
 impl Solution {
     pub fn check_if_pangram(sentence: String) -> bool {
         let available_letters: HashSet<char> = ('a'..='z').collect();
-        let content: HashSet<char> = sentence.chars().collect();
+        let content: HashSet<char> = sentence
+            .chars()
+            .collect();
+
         available_letters.is_subset(&content)
     }
 }
