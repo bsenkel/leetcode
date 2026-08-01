7311b415032c382f7a700f8cec90a96bc0fb1023
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-08-01 08:35:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (99.09%)

--- problems/2714-left-and-right-sum-differences/solution.rs
+++ problems/2714-left-and-right-sum-differences/solution.rs
@@ -2,7 +2,7 @@ impl Solution {
     pub fn left_right_difference(nums: Vec<i32>) -> Vec<i32> {
         let mut answer: Vec<i32> = Vec::new();
 
-        for (i, number) in nums.iter().enumerate() {
+        for (i, _) in nums.iter().enumerate() {
             let sum_right: i32 = (nums[i + 1..]).iter().sum();
             let sum_left: i32 = (nums[..i]).iter().sum();
 
