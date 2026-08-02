0f6ba4f2629597a6cc31dd2e570a04117030a169
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-08-02 18:28:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (42.86%)

--- problems/2727-number-of-senior-citizens/solution.rs
+++ problems/2727-number-of-senior-citizens/solution.rs
@@ -1,15 +1,11 @@
 impl Solution {
     pub fn count_seniors(details: Vec<String>) -> i32 {
-        let mut sum = 0;
-
-        for p in details {
-            let age_string: String = p.chars().skip(11).take(2).collect();
-            let age: i32 = age_string.parse().unwrap();
-            if age > 60 {
-                sum += 1;
-            }
-        }
-
-        sum
+        details
+            .iter()
+            .filter(|p| {
+                let age: i32 = p.chars().skip(11).take(2).collect::<String>().parse().unwrap();
+                age > 60
+            })
+            .count() as i32
     }
 }
