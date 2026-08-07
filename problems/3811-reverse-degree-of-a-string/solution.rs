3015281827e93a2a2f72b39c55701f54390045bc
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-08-07 19:17:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (25.00%)

---
+++ problems/3811-reverse-degree-of-a-string/solution.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn reverse_degree(s: String) -> i32 {
+        let lookup: HashMap<char, i32> = ('a'..='z').zip((1..=26).rev()).collect();
+
+        let mut sum = 0;
+        for (index, c) in s.chars().enumerate() {
+            sum += lookup.get(&c).unwrap() * (index as i32 + 1);
+        }
+
+        sum
+    }
+}
