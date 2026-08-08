c6f857338e6917185e05823f7063b05709f26e89
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-08-08 08:52:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (46.67%)

--- problems/3567-convert-date-to-binary/solution.rs
+++ problems/3567-convert-date-to-binary/solution.rs
@@ -1,13 +1,8 @@
 impl Solution {
     pub fn convert_date_to_binary(date: String) -> String {
-        let year_string: String = date.chars().take(4).collect();
-        let year: i32 = year_string.parse().unwrap();
-
-        let month_string: String = date.chars().skip(5).take(2).collect();
-        let month: i32 = month_string.parse().unwrap();
-
-        let day_string: String = date.chars().skip(8).take(2).collect();
-        let day: i32 = day_string.parse().unwrap();
+        let year: i32 = date[0..4].parse().unwrap();
+        let month: i32 = date[5..7].parse().unwrap();
+        let day: i32 = date[8..10].parse().unwrap();
 
         format!("{:b}-{:b}-{:b}", year, month, day)
     }
