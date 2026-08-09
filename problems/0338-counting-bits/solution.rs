3f2e9d24a1e104e55b133a78f4511944e60fb643
bsenkel <93093303+bsenkel@users.noreply.github.com>
2026-08-09 11:15:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.6 MB (79.35%)

---
+++ problems/0338-counting-bits/solution.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn count_bits(n: i32) -> Vec<i32> {
+        let mut output: Vec<i32> = Vec::new();
+        for i in 0..=n {
+            let mut number = i;
+            let mut count = 0;
+            while number > 0 {
+                if number & 1 == 1 {
+                    count += 1;
+                }
+                number >>= 1;
+            }
+
+            output.push(count as i32);
+        }
+
+        output
+    }
+}
